4d7cdfafa55d6e149d284f685f6ed13bc3b7c25f
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-11-28 17:13:00 +0530

    deduct picture

--- src/App.js
+++ src/App.js
@@ -3,35 +3,52 @@ import { useState,useEffect } from 'react';
 import { jwtDecode } from 'jwt-decode';
 function App() {
   const[user, setUser] = useState({})
-  const handleCallBack = (res) =>{
-    let user = jwtDecode(res.credential);
-    setUser(user);
-// console.log("Encoded token:" +res.credential)
-  }
+
+  const handleCallBack = (res) => {
+    console.log("Response from Google Sign-In:", res);
+    setUser({}) // Clear the previous user state
+    if (res && res.credential) {
+      const user = jwtDecode(res.credential);
+      setUser(user);
+      console.log("user",user)
+    } else {
+      console.error("No credentials found in the response");
+    }
+  };
 
   useEffect(()=>{
     /* global google */
+    if (google && !document.getElementById("SignIn").hasChildNodes()) {
     google.accounts.id.initialize({
       client_id:"1014479172456-qmqkp28bci38tdr9hmqtv06dupd3josc.apps.googleusercontent.com",
-      callback: handleCallBack
+      callback: handleCallBack,
+      auto_prompt: false, // Disable automatic prompts that trigger third-party cookies
     })
+    // Prevent automatic account selection
+  // google.accounts.id.disableAutoSelect();
+  google.accounts.id.prompt(); // Explicitly trigger the prompt instead of auto-prompt
+
+  if (!window.googleButtonRendered) {
     google.accounts.id.renderButton(
       document.getElementById("SignIn"),
       {theme:"outline", size:"large"}
-    )
+    )}
+  }
   },[])
   return (
     <>
-    <div>
+    <div className='flex flex-col items-center'>
     <h1 className="text-3xl mt-5 py-2 text-center">08- Learn With Podcasts
       
       </h1>
       <div id="SignIn"></div>
-      {user &&
-      <>
-      <img alt="User" src={user.picture} />
-      <h3>{user.name}</h3>
-      </> }
+      {user && user.name && user.picture && (
+  <>
+    <img alt="User" src={user.picture} />
+    <h3>{user.name}</h3>
+    <p>{user.email}</p>
+  </>
+)}
     </div>
    
       </>
